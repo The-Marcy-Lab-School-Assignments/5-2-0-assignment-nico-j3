9d85abea3adb457b1398efbe42706162c8b89743
nico-j3 <nicolejuarezflores@gmail.com>
2024-04-08 19:24:57 -0400

    6/7 and SR

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,13 +1,34 @@
 class Quadrilateral {
-
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1
+    this.side2 = side2
+    this.side3 = side3
+    this.side4 = side4
+  }
+  getPerimeter() {
+    let sum = this.side1 + this.side2 + this.side3 + this.side4
+    return sum
+  }
+}
+/////
+class Rectangle extends Quadrilateral {
+  constructor(length, width) {
+    super(length, width, length, width)
+  }
+  getArea() {
+    return (this.side1 * this.side2)
+  }
 }
 
-class Rectangle {
-
-}
-
-class Square {
-
+///
+class Square extends Rectangle {
+  constructor(length, width = length) {
+    super(length, width)
+  }
+  getDiagonal() {
+    let diagonal = this.side1 * Math.sqrt(2)
+    return diagonal
+  }
 }
 
 /* Be creative with this one! */
